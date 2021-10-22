f407cfd0c8319044961842490bb7801e6ad30b9b
Kipkorir2017 <pkobenj41@gmail.com>
2021-10-22 21:24:25 +0300

    add models

--- awwards/models.py
+++ awwards/models.py
@@ -1,3 +1,59 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
-# Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_pic = models.ImageField(upload_to = 'images/')
+    bio = models.TextField(max_length=500, blank=True)
+    contact = models.CharField(max_length=30, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+    @receiver(post_save, sender=User)
+    def update_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+        instance.profile.save()
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    def update_profile(cls, id):
+        Project.objects.get(user_id=id)
+
+
+class Project(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    title = models.CharField(max_length=150)
+    image = models.ImageField(upload_to = 'images/')
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True)
+    url = models.URLField(blank=True)
+    description = models.TextField(max_length=300, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    def save_project(self):
+        return self.save()
+
+    def delete_project(self):
+        self.delete()
+
+    @classmethod
+    def project_by_id(cls, id):
+        project = Project.objects.filter(id=id)
+        return project
+
+    @classmethod
+    def search_project(cls, name):
+        return cls.objects.filter(title__icontains=name).all()
+
