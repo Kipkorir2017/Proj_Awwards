86310b48e2020adc1b15a5b749190bd4610b4e6d
Kipkorir2017 <pkobenj41@gmail.com>
2021-10-22 23:51:58 +0300

    add test for Rate model

--- awwards/models.py
+++ awwards/models.py
@@ -59,16 +59,7 @@ class Project(models.Model):
 
 class Rates(models.Model):
     RATE_CHOICES = (
-        (1, '1'),
-        (2, '2'),
-        (3, '3'),
-        (4, '4'),
-        (5, '5'),
-        (6, '6'),
-        (7, '7'),
-        (8, '8'),
-        (9, '9'),
-        (10, '10'),
+        (1, '1'),(2, '2'),(3, '3'),(4, '4'),(5, '5'),(6, '6'),(7, '7'),(8, '8'),(9, '9'),(10, '10'),
     )
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     project = models.ForeignKey(Project, null=True, on_delete=models.CASCADE)
--- awwards/tests.py
+++ awwards/tests.py
@@ -1,11 +1,13 @@
 
 from django.test import TestCase
-from .models import Profile, Project
+from .models import Profile, Project,Rates
 from django.contrib.auth.models import User
 
 
 class ProfileTestCase(TestCase):
-    """Test for the profile model class"""
+    """
+    Test for the profile  class
+    """
     def setUp(self):
         self.user = User(username='kipkorir')
         self.user.save()
@@ -30,11 +32,8 @@ class ProjectTestClass(TestCase):
         self.new_profile = Profile(id=4, profile_pic='image.jpg', bio='Test profile',contact='0722345678',
                                     user=self.user)
         self.new_profile.save()
-        self.new_project = Project(image='image.png',title="image",url='http', description='test profile description', date='20/04/2020',
+        self.new_project = Project(image='image.png',title="image",url='http', description='test profile description', date='25/06/2021',
         profile=self.new_profile)
-
-
-  
 
 
     def test_instance_true(self):
@@ -42,8 +41,8 @@ class ProjectTestClass(TestCase):
 
     def test_save_project(self):
         self.new_project.save_project()
-        prj = Project.objects.all()
-        self.assertTrue(len(prj) == 1)
+        proj = Project.objects.all()
+        self.assertTrue(len(proj) == 1)
 
     def test_delete_project(self):
         self.new_project.save_project()
@@ -53,6 +52,19 @@ class ProjectTestClass(TestCase):
 
     def test_project_by_id(self):
         self.new_project.save_project()
-        prj = self.new_project.project_by_id(self.new_project.id)
+        proj = self.new_project.project_by_id(self.new_project.id)
         images = Project.objects.filter(id=self.new_project.id)
-        self.assertTrue(prj, images)   
+        self.assertTrue(proj, images)   
+
+class RatesTestCase(TestCase):
+    def setUp(self):
+        self.user = User(username='kipkorir')
+        self.user.save()
+        self.new_profile = Profile(id = 2,profile_pic='image.png',bio='test profile',user=self.user)
+        self.new_profile.save()
+        self.new_project = Project(image='image.png',title="image",url='http', description='test profile description', date='25/06/2021',
+        profile=self.new_profile)
+        self.rate = Rates(design='assssay',usability='good',content='good work',project = self.new_project, date="28/06/2021")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.rate, Rates))
