abd6b13de6aaa36865531ba68dc934767db1f128
Kipkorir2017 <pkobenj41@gmail.com>
2021-10-23 09:07:24 +0300

    add project view function

--- awwards/views.py
+++ awwards/views.py
@@ -1,6 +1,63 @@
+from django.http.response import HttpResponseRedirect
+from awwards.forms import ProjectForm, RatingsForm, SignUpForm, UpdateProfileForm, UpdateUserForm
 from django.shortcuts import render
-
+from awwards.models import Profile, Project, Rates
+from django.urls import reverse
 # Create your views here.
-def display_index(request):
 
-    return render(request,'index.html')
+def display_index(request):
+    profiles=Profile.objects.all()
+    projects=Project.objects.all()
+    return render(request,'index.html',{"profiles":profiles,"projects":projects})
+
+
+
+def profile(request):
+    current_user = request.user
+    projects = Project.objects.filter(user=current_user.id).all
+    return render(request, 'registration/profile.html', {"projects": projects})
+
+
+def project(request, id):
+    project = Project.objects.get(id=id)
+    reviews = Rates.objects.all()
+    return render(request, 'viewproject.html', {"project": project, "reviews": reviews})
+
+
+def view_project(request, id):
+    project = Project.objects.get(id=id)
+    rate = Rates.objects.filter(user=request.user, project=project).first()
+    ratings = Rates.objects.all()
+    rating_status = None
+    if rate is None:
+        rating_status = False
+    else:
+        rating_status = True
+    current_user = request.user
+    if request.method == 'POST':
+        form = RatingsForm(request.POST)
+        if form.is_valid():
+            design = form.cleaned_data['design']
+            usability = form.cleaned_data['usability']
+            content = form.cleaned_data['content']
+            review = Rates()
+            review.project = project
+            review.user = current_user
+            review.design = design
+            review.usability = usability
+            review.content = content
+            review.average = (
+                review.design + review.usability + review.content)/3
+            review.save()
+            return HttpResponseRedirect(reverse('viewProject', args=(project.id,)))
+    else:
+        form = RatingsForm()
+    params = {
+        'project': project,
+        'form': form,
+        'rating_status': rating_status,
+        'reviews': ratings,
+        'ratings': rate
+
+    }
+    return render(request, 'viewproject.html', params)
