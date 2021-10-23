12e0689696917763fb7f28f4136886e96e618a3c
Kipkorir2017 <pkobenj41@gmail.com>
2021-10-23 07:24:06 +0300

    Register class models on  the admin file

--- awwards/admin.py
+++ awwards/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from awwards.models import Profile,Rates,Project
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Project)
+admin.site.register(Rates)
